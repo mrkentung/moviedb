1b211c7d1cf375cc1f6f703105e40f66f8ffb39b
mrkentung <widiyanto.adhi@gmail.com>
2021-05-11 00:15:06 +0700

    fix: refactor and implements the components

--- pages/discover/popular.js
+++ pages/discover/popular.js
@@ -1,12 +1,11 @@
 import { Fragment, useState, useEffect } from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import { HiStar, HiOutlineStar } from 'react-icons/hi';
-import Stars from 'react-rating';
 import { animateScroll as scroll } from 'react-scroll';
 import { useQueryClient } from 'react-query';
+
 import { getPopular, usePopular } from '../../fetch/popular';
 import MainLayout from '../../layout/MainLayout';
+import MovieItem from '../../components/MovieItem';
 
 const Popular = () => {
   const queryClient = useQueryClient();
@@ -35,29 +34,13 @@ const Popular = () => {
               <div className="w-full grid grid-cols-4 gap-8">
                 {data.data.results?.map((item, i) => {
                   return (
-                    <div key={i} className="w-full cursor-pointer">
-                      <Link href={`/movie/${item.id}`}>
-                        <a>
-                          <img
-                            src={`https://image.tmdb.org/t/p/w342${item.poster_path}`}
-                            className="w-full rounded shadow-sm mb-2 object-cover hover:shadow-2xl transition-all duration-300 ease-in-out"
-                            style={{ height: '38rem' }}
-                            alt={item.original_title}
-                          />
-                          <p className="text-gray-600 text-base text-center">
-                            {item.original_title}
-                          </p>
-                          <div className="text-center">
-                            <Stars
-                              emptySymbol={<HiOutlineStar className="inline-block mr-2" />}
-                              fullSymbol={<HiStar className="inline-block mr-2" />}
-                              initialRating={item.vote_average / 2}
-                              readonly
-                            />
-                          </div>
-                        </a>
-                      </Link>
-                    </div>
+                    <MovieItem
+                      key={i}
+                      id={item.id}
+                      poster_path={item.poster_path}
+                      original_title={item.original_title}
+                      vote_average={item.vote_average}
+                    />
                   );
                 })}
               </div>
